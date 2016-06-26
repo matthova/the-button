36dd5b4db63bd0df86ce5ee5befd36ec3fa4eeda
hovanem <matt.hovanec@autodesk.com>
2016-06-26 06:56:59 -0700

    removed old commented out code

--- src/client/js/App.js
+++ src/client/js/App.js
@@ -9,7 +9,6 @@ export default class App extends React.Component {
   render() {
     const childrenComponents = React.Children.map(this.props.children, child => {
       // mapping through all of the children components in order to inject Clock app objects
-      // return React.cloneElement(child, this.props.params.hydraPrint);
       return React.cloneElement(child, clock);
     });
     return (
